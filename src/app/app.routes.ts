05eff2582e999207a3c2140a26edf1524087bee5
DinisSimoes <di.f.simoes@gmail.com>
2024-11-27 13:18:05 -0300

    add: app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PageAboutComponent } from './page-about/page-about.component';
+import { PageLabComponent } from './page-lab/page-lab.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     { path: '', component: HomePageComponent },
+     { path: 'sobre', component: PageAboutComponent },
+     { path: 'servicos', component: PageLabComponent },
+     { path: 'contato', component: PageAboutComponent },
+  ];
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
