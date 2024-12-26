ff7434936f4c2a796e338a7e8eda4988cf8cb4f2
DinisSimoes <di.f.simoes@gmail.com>
2024-12-26 16:28:28 -0300

    fix page about

--- src/app/UI/page-contact/page-contact.component.ts
+++ src/app/UI/page-contact/page-contact.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { FormsModule } from '@angular/forms';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-page-contact',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, FormsModule, InputTextareaModule, FloatLabelModule, InputTextModule],
   templateUrl: './page-contact.component.html',
   styleUrl: './page-contact.component.scss'
 })
 export class PageContactComponent {
-
+  value:string="teste";
 }
