ab4a89784a33256519a2ee426bc3d34291455e59
DinisSimoes <di.f.simoes@gmail.com>
2024-12-26 17:56:37 -0300

    #5 textos na pagina

--- src/app/Models/translations.ts
+++ src/app/Models/translations.ts
@@ -1,10 +1,22 @@
 export const TRANSLATIONS: Record<string, Record<string, string>> = {
     PT:{
         buttonDownloadCV: "Meu CV",
-        conteudo: "Conteudo"
+        conteudo: "Conteudo",
+        lab_title: "Bem-vindo(a) ao meu laboratório. Aqui, a vida é programada.",
+        lab_subtitle: "Criação e destruição, beleza e horror. A tecnologia pode ser bela, mas pouco perigosa.",
+        lab_content: "No meu laboratório, essas forças se entrelaçam. Assim como Frankenstein, às vezes me questiono sobre os limites da minha criação.",
+        lab_content2: "Cada projeto é uma nova criatura que surge das profundezas da minha mente, um organismo digital com vida própria.",
+        lab_caution: "Atenção: Alguma coisa está sendo criada atualmente, não se assuste... Algumas das criaturas são belas, outras... bem, precisariam de um pouco mais de trabalho",
+        lab_button: "Revele as minhas Criaturas"
     },
     EN:{
         buttonDownloadCV: "My CV",
-        conteudo:"Content"
+        conteudo:"Content",
+        lab_title: "Welcome to my laboratory. Here, life is programmed.",
+        lab_subtitle: "Creation and destruction, beauty and horror. Technology can be beautiful, but not very dangerous.",
+        lab_content: "In my laboratory, these forces intertwine. As in Frankenstein, sometimes I wonder about the limits of my creation.",
+        lab_content2: "Each project is a new creature that emerges from the depths of my mind, a digital organism with its own life.",
+        lab_caution: "Warning: Something is being created right now, don't be afraid... Some creatures are beautiful, others... well, they would need a little more work",
+        lab_button: "Reveal my Creatures"
     }
 }
